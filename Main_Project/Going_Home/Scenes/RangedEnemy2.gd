05452671a1e1031404730e793e7581b35eb9db3e
Jonathan Barboza Morales <jonathan10barboza@gmail.com>
2023-04-11 17:12:23 -0600

    Projectile Enemies
    -Added enemies with the ability to shoot projectiles repeatedly in a straight line
    -Tried adding homing enemies, did not succeed
    -Hitbox on projectiles don't respawn player
    -All code associated with RangedEnemy is good to use, only issue is the hitbox
    -Code associated with RangedEnemy2 has extra features that don't work, so I suggest just using RangedEnemy code for now to avoid repeated, unnecesary code

---
+++ Main_Project/Going_Home/Scenes/RangedEnemy2.gd
@@ -0,0 +1,77 @@
+extends KinematicBody2D
+
+var distFromPlay : float = 0
+var inView : bool = false
+var shootCoolDown : int = 90
+var coolDone : bool = false
+var velocity : Vector2 = Vector2()
+onready var sprite : Sprite = $InActive
+
+func _ready():
+	
+	$Active.hide()
+	velocity.x = 0
+	velocity.y = 0
+
+func _physics_process(_delta):
+
+	coolDone = false
+	
+	checkView() #not working
+		
+	if shootCoolDown < 0:
+		coolDone = true
+		
+	display() #not working
+	
+	if coolDone:
+		rangedAttack()
+		shootCoolDown = 90
+		
+	
+	shootCoolDown -= 1
+	
+
+
+func rangedAttack():
+	
+	var projectile = get_parent().get_node("Projectile2")
+	var player = get_parent().get_node("Player")
+	
+	projectile.position = projectile.initPos
+	
+	var direction = (player.position - self.position).normalized()
+	
+	projectile.launch(Vector2(1,0).normalized())
+	
+	
+
+func getPlayDist():
+	
+	var enemyX : float = self.get_position().x
+	var enemyY : float = self.get_position().y
+	var playX : float = get_parent().get_node("Player").get_position().x
+	var playY : float = get_parent().get_node("Player").get_position().y
+	
+	var xDiff : float = enemyX - playX
+	var yDiff : float = enemyY - playY
+	
+	return sqrt(( xDiff * xDiff) + ( yDiff * yDiff ))
+	
+func display():
+	if inView:
+		sprite = $Active
+		$InActive.hide()
+		$Active.show()
+	else:
+		sprite = $InActive
+		$Active.hide()
+		$InActive.show()
+
+func checkView():
+	distFromPlay = getPlayDist()
+	
+	if distFromPlay <= 200:
+		inView = true
+	else:
+		inView = false
