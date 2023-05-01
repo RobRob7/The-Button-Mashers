1bc01647c5ecf1103008b0d78846c409ef3a69f6
Robert J. Armendariz <rjames11@nmsu.edu>
2023-04-30 21:42:05 -0600

    level 2 is done

--- Main_Project/Going_Home/Scripts/levels/Level2.gd
+++ Main_Project/Going_Home/Scripts/levels/Level2.gd
@@ -7,6 +7,6 @@ func _ready():
 	player = get_node("Player")
 	playerCamera = player.get_child(2)
 	playerCamera.limit_left = 0
-	playerCamera.limit_right = 1896
+	playerCamera.limit_right = 4000
 	playerCamera.limit_top = 0
 	playerCamera.limit_bottom = 1504
