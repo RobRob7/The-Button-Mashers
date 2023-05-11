6e4f31fa7736e5b67d4a8275bebba9bbb001fd64
jcengstrom <jcengstrom1@gmail.com>
2023-05-11 00:32:40 -0600

    Added resolution
    Resolution settings

---
+++ Main_Project/Going_Home/Scripts/MainMenu/GraphicsMenu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+var resolutions = [
+	Vector2(800, 600),
+	Vector2(1024, 768),
+	Vector2(1280, 720),
+	Vector2(1920, 1080),
+	Vector2(2560, 1440),
+	Vector2(3840, 2160)
+]
+
+func _ready():
+	var dropdown = get_node("VBoxContainer/ResolutionButton")
+	for resolution in resolutions:
+		dropdown.add_item(str(resolution.x) + "x" + str(resolution.y))
+
+
+func _on_ResolutionButton_item_selected(index):
+	OS.window_size = resolutions[index]
+
+
+func _on_Back_Button_pressed():
+	get_tree().change_scene("res://Scenes/MainMenu/OptionsMainMenu.tscn")
